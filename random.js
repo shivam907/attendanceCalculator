24794ace917f88496229f6cc10d072d0f91b57ea
shivam907 <shivamkaushal907@gmail.com>
2023-05-10 02:52:37 +0530

    first page logic done

---
+++ random.js
@@ -0,0 +1,5 @@
+const subject = "OS";
+const totalClassesInWeek = 4;
+const totalClassesInMonth = totalClassesInWeek * 4;
+const classesNeededWithoutMedical = Math.ceil((75 * totalClassesInMonth) / 100);
+const classesNeededWithMedical = Math.ceil((65 * totalClassesInMonth) / 100);
